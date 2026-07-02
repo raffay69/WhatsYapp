27ac806f1c2f7a6c484cca1ed8fdd8d55a5c6ce6
Mohd Abdul raffay <mohdabdulraffay@Mohds-MacBook-Air.local>
2026-07-03 01:21:08 +0530

    fixed: some frontend hosting issues

--- frontend/build.ts
+++ frontend/build.ts
@@ -23,3 +23,5 @@ const result = await Bun.build({
 for (const output of result.outputs) {
   console.log(` ${path.relative(process.cwd(), output.path)}  ${(output.size / 1024).toFixed(1)} KB`);
 }
+
+await Bun.$`cp -R public dist/public`;
